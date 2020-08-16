40a616079bf9667b5162f1c6750a90b209c353f1
rid3thespiral <davidebattaglia1993@gmail.com>
2020-08-16 11:46:31 +0200

    hex encoding

--- client.js
+++ client.js
@@ -1,25 +1,27 @@
 const url = "https://mainnet.infura.io/v3/293d4a9cac5047e6b9ed5eb54fac669a";
 const startingBlock = 6607985;
 const endingBlock = 7028323;
-const axios = require('axios')
+const axios = require('axios');
+const startingBlockHex = '0x'+startingBlock.toString(16);
+const endingBlockHex = '0x'+endingBlock.toString(16);
+
 
 axios.post(url, {
     "jsonrpc":"2.0",
     "method":"eth_getLogs",
-    "params":[{"blockHash": "0x7c5a35e9cb3e8ae0e221ab470abae9d446c3a5626ce6689fc777dcffcab52c70", 
-    "topics":["0x241ea03ca20251805084d27d4440371c34a0b85ff108f6bb5611248f73818b80"]}],
-    "id":1
-})
+    "params":[
+        {
+            "fromBlock": "0x64d471",
+            "toBlock": "0x64d472",
+        }],
+        "id":1
+    }
+)
 .then((res) => {
   console.log(`statusCode: ${res.statusCode}`)
-  console.log(res)
+  console.log(res.data)
 })
 .catch((error) => {
   console.error(error)
 })
 
-function countBirths(startingBlock, endingBlock){
-    let count = 0;
-
-    return count;
-}
